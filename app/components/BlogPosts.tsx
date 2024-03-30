ce9abe87f68536c07417449d5b60b428fa421590
maxwellmendenhall <mmaxwell.mmendenhall@gmail.com>
2024-03-30 03:16:31 -0400

    fixed spelling

--- app/components/BlogPosts.tsx
+++ app/components/BlogPosts.tsx
@@ -47,7 +47,7 @@ const BlogPosts: React.FC = () => {
                 <div className="mx-auto max-w-2xl lg:mx-0">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Maxwell Mendenhall</h2>
                     <p className="mt-2 text-lg leading-8 text-gray-600">
-                        Studying Computer Science, White Collar Crime, and Computer Secrutiy.
+                        Studying Computer Science, White Collar Crime, and Computer Security.
                     </p>
                 </div>
                 <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
